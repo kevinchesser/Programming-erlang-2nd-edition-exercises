6d9fd61f7406c5fc2aa5f6c9f4f65b40aa670864
Kevin Chesser <kevin.chesser94@gmail.com>
2017-01-26 16:10:21 -0500

    add file_server

---
+++ Chapter2/file_server.erl
@@ -0,0 +1,15 @@
+-module(file_server).
+-export([start/1, loop/1]).
+
+start(Dir) -> spawn(file_server, loop, [Dir]). %%Module, function, List of arguments
+
+loop(Dir) ->
+    receive
+        {Client, list_dir} -> %%From, Message - Client gets the pid the message is from, list_dir is the message
+            Client ! {self(), file:list_dir(Dir)}; 
+            %%Pid ! Msg - Send message to the Pid - self() argument identified the process sending the message
+        {Client, {get_file, File}} ->
+            Full = filename:join(Dir, File),
+            Client ! {self(), file:read_file(Full)}
+    end,
+    loop(Dir).
