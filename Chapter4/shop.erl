f1024202fc5bb404a5954011baf3ca42858b52c9
Kevin Chesser <kevin.chesser94@gmail.com>
2017-01-27 16:40:06 -0500

    add total function to shop

--- Chapter4/shop.erl
+++ Chapter4/shop.erl
@@ -1,7 +1,7 @@
 -module(shop).
--export([cost/1]).
+-export([cost/1, total/1]).
 
-cost(orange) ->
+cost(oranges) ->
     5;
 cost(newspaper) ->
     8;
@@ -11,3 +11,8 @@ cost(pears) ->
     9;
 cost(milk) ->
     7.
+
+total([{What, N}|T]) ->
+    cost(What) * N + total(T);
+total([]) ->
+    0.
