82864deca7046cc8c2f0522f97e23b5248c48849
Kevin Chesser <kevin.chesser94@gmail.com>
2017-01-27 17:36:08 -0500

    add quicksort function

--- Chapter4/lib_misc.erl
+++ Chapter4/lib_misc.erl
@@ -5,3 +5,10 @@ for(Max, Max, F) ->
     [F(Max)];
 for(I, Max, F) ->
     [F(I)|for(I+1, Max, F)].
+
+quicksort([]) ->
+    [];
+quicksort([Pivot|T]) ->
+    quicksort([X || X <- T, X < Pivot])
+    ++ [Pivot] ++
+    quicksort([X || X <- T, X >= Pivot]).
