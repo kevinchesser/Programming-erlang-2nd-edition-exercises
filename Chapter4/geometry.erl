caa100a8b3a14a611b61a76efde5112bd3c35b56
Kevin Chesser <kevin.chesser94@gmail.com>
2017-01-27 15:49:36 -0500

    add tests for geometry

--- Chapter4/geometry.erl
+++ Chapter4/geometry.erl
@@ -1,5 +1,10 @@
 -module(geometry).
--export([area/1]).
+-export([test/0, area/1]).
+
+test() ->
+    50 = area({rectangle, 10, 5}),
+    169 = area({square, 13}),
+    tests_worked.
 
 area({rectangle, Width, Height}) ->
     Width * Height;
