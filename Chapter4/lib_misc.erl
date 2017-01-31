9741247fad941d5a58aecf996c130154be85e201
kevinchesser <kevin.chesser94@gmail.com>
2017-01-30 22:07:59 -0500

    add odds_and_evens functions

--- Chapter4/lib_misc.erl
+++ Chapter4/lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([for/3,pythag/1]).
+-export([for/3,pythag/1,odds_and_evens2/1]).
 
 for(Max, Max, F) ->
     [F(Max)];
@@ -24,4 +24,22 @@ pythag(N) ->
 perms([]) ->
     [[]];
 perms(L) ->
-    [[H|T] | | H <- L, T <- perms(L--[H])].
+    [[H|T] || H <- L, T <- perms(L--[H])].
+
+odds_and_evens1(L) ->
+    %Inefficent solution since the list must be travered twice
+    Odds = [X || X <- L, (X rem 2) =:= 1],
+    Evens = [Y || Y <- L, (Y rem 2) =:= 0],
+    {Odds, Evens}.
+
+odds_and_evens2(L) -> %More efficient since it only traverses the list once
+    odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H|T], Odds, Evens) ->
+    case (H rem 2) of
+        1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+        0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+    end;
+odds_and_evens_acc([], Odds, Evens) ->
+    {lists:reverse(Odds), lists:reverse(Evens)}.
+    %Only need to reverse if you care about the lists being in the right order
