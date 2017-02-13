f72a1d077469567d81f079874344ec4254f407c1
Kevin Chesser <kevin.chesser94@gmail.com>
2017-02-13 17:14:25 -0500

    add utilities for reading from stdin

---
+++ Utilities/io.erl
@@ -0,0 +1,32 @@
+main() ->
+    { ok, [N]} = io:fread("", "~d"),
+    io:format("~p~n", [N]),
+    Nums = read_data([]),
+    io:format("~p~n", [Nums]).
+
+read_data(Numbers) -> 
+   case io:fread("", "~d") of
+       eof ->
+           lists:reverse(Numbers);
+       {error, _} ->
+           lists:reverse(Numbers);
+       {ok, [X]} ->
+           read_data([X|Numbers])
+   end.
+
+main() ->
+
+        { ok, [N]} = io:fread("", "~d"),
+            A = read_2darray(N,N,"~d"),
+                true.
+
+read_array(0,D) -> [];
+read_array(N,D) ->
+       {ok, [X]} = io:fread("", D),
+          [X | read_array(N-1,D)].
+
+read_2darray(0,M,D) -> [];
+read_2darray(N,M,D) ->
+       Q=read_array(M,D),
+          [Q | read_2darray(N-1,M,D)].
+
